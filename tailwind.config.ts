aee88ab799880f3df5fce03e7e88a8d2654a3d37
VcelakMarek <vcelak.marek@gmail.com>
2025-01-14 15:11:48 +0100

    add colors to tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,4 @@
+import forms from "@tailwindcss/forms";
 import type { Config } from "tailwindcss";
 
 export default {
@@ -9,10 +10,21 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        "purple": "var(--purple)",
+        "purple-hover": "var(--purple-hover)",
+        "red":"var(--red)",
+        "red-hover":"var(--red-hover)",
+        "dark-blue":"var(--dark-blue)",
+        "blue":"var(--blue)",
+        "light-blue":"var(--light-blue)",
+        "light-grey":"var(--light-grey)",
+        "grey":"var(--grey)",
+        "black":"var(--black)",
+        "black2":"var(--black2)",
+        "light-bg":"var(--light-bg)"
       },
     },
   },
-  plugins: [],
+  plugins: [forms],
+
 } satisfies Config;
