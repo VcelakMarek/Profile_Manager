cdc353e92935b718b988244cfd1bd408505b450e
VcelakMarek <vcelak.marek@gmail.com>
2025-01-17 23:18:41 +0100

    fix pathName in middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server"
 import { COOKIE_NAME } from "@utils/constants"
 
 export function middleware(request: NextRequest) {
-  if (request.nextUrl.pathname.startsWith("/profiles")) {
+  if (request.nextUrl.pathname.startsWith("/")) {
     if (!request.cookies.has(COOKIE_NAME)) {
       return NextResponse.redirect(new URL("/signin", request.url))
     }
