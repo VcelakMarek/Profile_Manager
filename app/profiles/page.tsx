b7a0f0a3e4afdda661107140e279deae0ec38879
VcelakMarek <vcelak.marek@gmail.com>
2025-01-17 23:31:28 +0100

    add ProfilesPage.tsx

---
+++ app/profiles/page.tsx
@@ -0,0 +1,40 @@
+import prisma from "@lib/db"
+import PageHeader from "@components/Header"
+import Profile from "@components/Profile"
+import { getCurrentUser } from "@utils/users"
+
+const ProfilesPage = async () => {
+  const { id: currentUserId } = await getCurrentUser()
+  const profiles = await prisma.profile.findMany({
+    select: {
+      id: true,
+      firstName: true,
+      lastName: true,
+      dateOfBirth: true,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  })
+
+  return (
+    <div className="flex h-screen w-screen flex-col bg-light-bg">
+      <PageHeader isProfileList profileCount={profiles.length} />
+      <main className="mt-16 grid place-items-center">
+        {profiles.length > 0 ? (
+          profiles.map((profile) => (
+            <Profile
+              key={profile.id}
+              profileData={profile}
+              isCurrentUser={profile.id === currentUserId}
+            />
+          ))
+        ) : (
+          <p>Profiles not found</p>
+        )}
+      </main>
+    </div>
+  )
+}
+
+export default ProfilesPage
