25daad06d3d11ecd9f3b99c1a97e8a2f3e1770f1
VcelakMarek <vcelak.marek@gmail.com>
2025-01-17 23:41:04 +0100

    add profileHandlers.ts

---
+++ utils/profileHandlers.ts
@@ -0,0 +1,75 @@
+import { useRouter } from "next/navigation"
+import {
+  EditProfileFormValues,
+  CreateProfileFormValues,
+} from "@schemas/profileSchemas"
+import { editProfile, createProfile, deleteProfile } from "@actions/profile"
+
+type HandleEditProfileProps = {
+  id: string
+  data: EditProfileFormValues
+  isSetup?: boolean
+  router: ReturnType<typeof useRouter>
+}
+
+type HandleCreateProfileProps = {
+  data: CreateProfileFormValues
+  router: ReturnType<typeof useRouter>
+}
+
+export const handleEditProfile = async ({
+  id,
+  data,
+  isSetup,
+  router,
+}: HandleEditProfileProps) => {
+  try {
+    const formData = new FormData()
+    for (const key in data) {
+      const value = data[key as keyof EditProfileFormValues]
+      formData.append(key, value || "")
+    }
+
+    await editProfile(id, formData)
+    alert("Profile updated successfully!")
+
+    if (isSetup) {
+      router.replace("/")
+    } else {
+      router.replace(`/profiles/${id}`)
+    }
+  } catch (error: unknown) {
+    console.error("Unexpected error", error)
+    alert("An unexpected error occurred.")
+  }
+}
+
+export const handleCreateProfile = async ({
+  data,
+  router,
+}: HandleCreateProfileProps) => {
+  try {
+    const formData = new FormData()
+    for (const key in data) {
+      const value = data[key as keyof CreateProfileFormValues]
+      formData.append(key, value || "")
+    }
+    await createProfile(formData)
+    alert("Profile Created successfully!")
+
+    router.replace(`/profiles`)
+  } catch (error: unknown) {
+    console.error("Unexpected error", error)
+    alert("An unexpected error occurred.")
+  }
+}
+
+export const handleDeleteProfile = async (id: string) => {
+  try {
+    await deleteProfile(id)
+    alert("Profile deleted successfully!")
+  } catch (error) {
+    console.error("Error deleting profile:", error)
+    alert("An error occurred while deleting the profile. Please try again.")
+  }
+}
