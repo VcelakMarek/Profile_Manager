bf9462c634507949805cebe251ccda4de0ce3fba
VcelakMarek <vcelak.marek@gmail.com>
2025-01-18 09:48:15 +0100

    add import type

--- app/profiles/page.tsx
+++ app/profiles/page.tsx
@@ -2,6 +2,7 @@ import prisma from "@lib/db"
 import PageHeader from "@components/Header"
 import Profile from "@components/Profile"
 import { getCurrentUser } from "@utils/users"
+import type { ProfileTypes } from "@/types/ProfileTypes"
 
 const ProfilesPage = async () => {
   const { id: currentUserId } = await getCurrentUser()
@@ -22,7 +23,7 @@ const ProfilesPage = async () => {
       <PageHeader isProfileList profileCount={profiles.length} />
       <main className="mt-16 grid place-items-center">
         {profiles.length > 0 ? (
-          profiles.map((profile) => (
+          profiles.map((profile: ProfileTypes) => (
             <Profile
               key={profile.id}
               profileData={profile}
